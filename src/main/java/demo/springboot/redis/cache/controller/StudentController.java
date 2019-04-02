eee0603e5094900b33f8ca291b50f1022a158be8
softgen007 <softgen007@gmail.com>
2019-04-01 22:38:39 -0700

    updated controller

--- src/main/java/demo/springboot/redis/cache/controller/StudentController.java
+++ src/main/java/demo/springboot/redis/cache/controller/StudentController.java
@@ -3,6 +3,9 @@ package demo.springboot.redis.cache.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import ch.qos.logback.classic.Logger;
 import demo.springboot.redis.cache.model.Student;
 import demo.springboot.redis.cache.repo.StudentRepository;
 
@@ -31,6 +35,7 @@ public class StudentController {
 		return studentRepository.findAll();
 	}
 	
+	@Cacheable(value = "studentCache", key = "#id")
 	@GetMapping("/student/{id}")
 	public Student getStudent(@PathVariable String id){
 		return studentRepository.findById(id).orElse(null);
@@ -42,6 +47,7 @@ public class StudentController {
 		studentRepository.save(student);
 	}
 	
+	@CacheEvict(value = "studentCache", key = "#id")
 	@DeleteMapping("/student/{id}")
 	public void deleteStudent(@PathVariable String id){
 		studentRepository.deleteById(id);
--- src/main/java/demo/springboot/redis/cache/repo/StudentRepository.java
+++ src/main/java/demo/springboot/redis/cache/repo/StudentRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import demo.springboot.redis.cache.model.Student;
 
+
 public interface StudentRepository extends JpaRepository<Student, String> {
 	Optional<Student> findById(String id);
 
